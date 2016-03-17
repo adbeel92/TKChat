c72e3813b71192e5a0aa09ae0b5167d949ada3b7
Eduardo Arenas <eduardo.arenas@tektonlabs.com>
2016-03-17 17:00:35 -0500

    Fix

--- index.js
+++ index.js
@@ -118,6 +118,6 @@ io.on("connection", function(socket){
 });
 
 
-http.listen(app.get("port"), app.get("host"), function() {
-  console.log("Server up and running. Go to http://" + app.get("host") + ":" + app.get("port"));
+app.listen(app.get("port"), function() {
+  console.log("Server up and running. Go to http://HOST:" + app.get("port"));
 });
